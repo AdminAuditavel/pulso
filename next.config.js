dc0aac9fcec3232ef24633f4c3b9cf019ef5d4aa
AdminAuditavel <admin@auditavel.com>
2025-12-24 07:47:58 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: [] // adicionar domínios de imagens se necessário
-  }
-};
-
-module.exports = nextConfig;
